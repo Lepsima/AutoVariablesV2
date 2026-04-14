b823708677eb080541128d59732ee885098bf596
lepsima <lepsima@proton.me>
2026-04-14 23:59:02 +0200

    Remove ignored files

--- AutoVariablesApp/obj/Generated/AutoVariablesV2/ClassGenerator.ClassGenerator/ASV/Utils/Interfaces.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Lepsima.ASV {
-    public interface IAutoUnit {}
-    public interface IAutoUnit2 {}
-    public interface IAutoUnit3 {}
-    
-    public interface IAutoUnitUI {}
-    public interface IAutoUnitUI2 {}
-    public interface IAutoUnitUI3 {}
-}
--- AutoVariablesApp/obj/Generated/AutoVariablesV2/ClassGenerator.ClassGenerator/ASV/Utils/Interfaces.g.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Generated.Units {
-    public interface IAutoUnit {}
-    public interface IAutoUnit2 {}
-    public interface IAutoUnit3 {}
-    
-    public interface IAutoUnitUI {}
-    public interface IAutoUnitUI2 {}
-    public interface IAutoUnitUI3 {}
-}
